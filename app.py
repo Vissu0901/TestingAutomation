7b66c13808202a1151c8cc98c21fce830e8d1d78
Vissu0901 <viswanath.varikuti09@gmail.com>
2023-05-28 14:04:18 +0530

    deploy - 28-05-2023

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def dummy():
-    return "working"
+#https://testingautomation.onrender.com/
+
+@app.get('/')
+def home_page():
+    return {"message":"Welcome to Testing Automation site"}
+
+@app.get('/create-user')
+def create_user():
+    return {"message":"still in progress"}
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=5000)
